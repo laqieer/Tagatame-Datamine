1f4ba83f1a75c977b30be4192803f7abb2b198b4
laqieer <laqieer@126.com>
2024-10-17 23:17:18 +0800

    List all managed assets

---
+++ 0_list_all_assets.py
@@ -0,0 +1,24 @@
+import UnityPy
+import os
+
+asset_path = "DMM/tagatame/new_win32/"
+asset_list = "asset_list.csv"
+
+with open(asset_list, "w", encoding="utf-8") as f:
+    f.write("Bundle,Path,Type,Name\n")
+    for file in os.listdir(asset_path):
+        # check if the filename is a valid number in hexidecimal
+        try:
+            int(file, 16)
+        except:
+            continue
+        
+        # Load the bundle
+        print(f"Loading {file}...")
+        env = UnityPy.load(os.path.join(asset_path, file))
+        
+        # Iterate through the objects in the bundle
+        for path, obj in env.container.items():
+            if obj.type.name in ("Texture2D", "Sprite", "TextAsset", "Font", "MonoBehaviour"):
+                data = obj.read()
+                f.write(','.join([file, path, obj.type.name, data.name]) + '\n')
