5970b9ea83abccdfc20097d840459e0d866d4c38
laqieer <laqieer@126.com>
2024-11-03 19:29:59 +0800

    Split unmanaged assets into submodule

--- 2_download_unmanaged_assets.py
+++ 2_download_unmanaged_assets.py
@@ -12,14 +12,15 @@ with open('DMM/tagatame/new_win32/UnmanagedAssetList', 'r', encoding='utf-8') as
         line = line.strip()
         if line:
             path, size = line.split('\t')
-            if os.path.exists(path) and os.path.getsize(path) == int(size):
+            save_path = os.path.join('UnmanagedAsset/', path)
+            if os.path.exists(save_path) and os.path.getsize(save_path) == int(size):
                     continue
             url = f'https://alchemist-dlc2.gu3.jp/assets_ex/{assets_ex}/{path}'
-            print(f'Downloading {url} to {path}')
+            print(f'Downloading {url} to {save_path}')
             response = requests.get(url)
             if response.status_code == 200:
-                os.makedirs(os.path.dirname(path), exist_ok=True)
-                with open(path, 'wb') as f:
+                os.makedirs(os.path.dirname(save_path), exist_ok=True)
+                with open(save_path, 'wb') as f:
                     f.write(response.content)
             else:
                 warnings.warn(f'Failed to download {path}: {response.status_code} {response.text}')
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Tagatame-Datamine
 Datamine of Tagatame (タガタメ / 誰ガ為のアルケミスト / The Alchemist Code)
+
+`UnmanagedAsset` is splited to [its own repo](https://gitlab.com/laqieer/the-alchemist-codes-unmanaged-asset) due to storage size limit.
